d7698f94c991522ea54406ae9e18c19c19b91709
eric <ontime.c23@gmail.com>
2025-06-19 08:24:45 +0300

    modified:   src/services/jengaApi.ts

--- src/services/jengaApi.ts
+++ src/services/jengaApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "../app/store";
 
-const baseURL = import.meta.env.VITE_JENGA_API_URL;
+const baseURL = import.meta.env.VITE_JENGA_ACCOUNT_API_URL;
 const apiKey = import.meta.env.VITE_JENGA_API_KEY;
 
 export const jengaApi = createApi({
